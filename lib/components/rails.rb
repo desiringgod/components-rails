b1c731c42974f6949c2e96e1b4ec98325b8b3941
Ben Hutton <benhutton@gmail.com>
2017-07-31 14:32:57 -0400

    Run `bundler gem .`

---
+++ lib/components/rails.rb
@@ -0,0 +1,7 @@
+require "components/rails/version"
+
+module Components
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/components/rails/version.rb
@@ -0,0 +1,5 @@
+module Components
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
