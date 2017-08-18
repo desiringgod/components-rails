e4cc0252dbfe9c2b6fec49b80dad194227eb7626
Joe Osburn <joe+desiringgod@jnodev.com>
2017-08-18 10:28:56 -0500

    Create simple feature spec

---
+++ spec/features/rendering_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'rendering' do
+  let(:component_class) do
+    Class.new(Components::Rails::Component) do
+      def cache_key
+        an_option
+      end
+
+      def show
+        render inline: <<~eos
+          some content
+
+          <%= an_option %>
+
+          more content
+        eos
+      end
+
+      def an_option
+        options[:an_option]
+      end
+      helper_method :an_option
+    end
+  end
+
+  before { Object.const_set("My#{Time.now.subsec.numerator}Component", component_class) }
+
+  it 'renders a component' do
+    expect(component_class.render_action(:show, ActionView::Base.new, an_option: 'value')).to eq(<<~eos)
+      some content
+
+      value
+
+      more content
+    eos
+  end
+end
