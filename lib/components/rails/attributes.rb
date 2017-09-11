33b8905273231073c827b5c73f3e8d9af703d363
Joe Osburn <joe+desiringgod@jnodev.com>
2017-09-11 09:35:28 -0500

    Add support for default attributes values

--- lib/components/rails/attributes.rb
+++ lib/components/rails/attributes.rb
@@ -3,8 +3,15 @@
 module Components
   module Rails
     module Attributes
+      extend ActiveSupport::Concern
+
+      included do
+        class_attribute :defaults
+        self.defaults = {}
+      end
+
       def _render_template(options)
-        super(options.merge(locals: attributes))
+        super(options.merge(locals: defaults.merge(attributes)))
       end
     end
   end
