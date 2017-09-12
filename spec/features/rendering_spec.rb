4940260e799d47c09846591e645d92bd462cb7c5
Joe Osburn <joe+desiringgod@jnodev.com>
2017-09-11 21:24:16 -0500

    Refactor rendering spec to use magic component helper method

--- spec/features/rendering_spec.rb
+++ spec/features/rendering_spec.rb
@@ -2,14 +2,20 @@
 
 require 'rails_helper'
 
-RSpec.describe 'rendering' do
-  let(:component_class) do
-    Class.new(Components::Rails::Component) do
-      def cache_key
-        [attributes[:an_attribute], block_content]
-      end
+RSpec.describe 'Component rendering' do
+  let(:component_class) { Class.new(Components::Rails::Component) { def cache_key; '123'; end } }
+  let(:component_class_name) { "my_#{Time.now.subsec.numerator}_component" }
+  let(:action_view) { ActionView::Base.new }
 
-      def show
+  before do
+    ActionView::Base.include Components::Rails::ActionView
+    Object.const_set(component_class_name.camelize, component_class)
+    Components::Rails::ActionView.define_component_helper(component_class_name)
+  end
+
+  describe 'with attribute' do
+    before do
+      component_class.send(:redefine_method, :show) do
         render inline: <<~eos
           some content
 
@@ -18,8 +24,52 @@ RSpec.describe 'rendering' do
           more content
         eos
       end
+    end
 
-      def index
+    it 'works' do
+      expect(action_view.render(inline: "template <%= #{component_class_name}(an_attribute: 'val') %>")).to eq(<<~eos)
+        template some content
+
+        val
+
+        more content
+      eos
+    end
+
+    describe 'setting defaults' do
+      before { component_class.defaults = {an_attribute: 'default value'} }
+
+      context 'with attribute' do
+        it 'uses the value' do
+          expect(
+            action_view.render(inline: "template <%= #{component_class_name}(an_attribute: 'given val') %>")
+          ).to eq(<<~eos)
+            template some content
+
+            given val
+
+            more content
+          eos
+        end
+      end
+
+      context 'without attribute' do
+        it 'uses the default value' do
+          expect(action_view.render(inline: "template <%= #{component_class_name} %>")).to eq(<<~eos)
+            template some content
+
+            default value
+
+            more content
+          eos
+        end
+      end
+    end
+  end
+
+  describe 'with block' do
+    before do
+      component_class.send(:redefine_method, :show) do
         render inline: <<~eos
           some content
 
@@ -28,85 +78,33 @@ RSpec.describe 'rendering' do
           more content
         eos
       end
-    end
-  end
 
-  before { Object.const_set("My#{Time.now.subsec.numerator}Component", component_class) }
-
-  it 'renders a component' do
-    expect(component_class.render_action(:show, ActionView::Base.new, an_attribute: 'value')).to eq(<<~eos)
-      some content
-
-      value
-
-      more content
-    eos
-  end
-
-  it 'renders a component with a block' do
-    expect(component_class.render_action(:index, ActionView::Base.new) do
-      'content from block'
-    end).to eq(<<~eos)
-      some content
-
-      content from block
-
-      more content
-    eos
-  end
-
-  describe 'block_content in #cache_key' do
-    subject do
-      component_class.new(nil, ActionView::Base.new, :show, an_attribute: :value, block: proc { 'the block' })
+      component_class.send(:redefine_method, :cache_key) do
+        [attributes[:an_attribute], block_content]
+      end
     end
 
     it 'works' do
-      expect(subject.cache_key).to eq([:value, 'the block'])
+      expect(
+        action_view.render(
+          inline: "template <%= #{component_class_name}(an_attribute: 'value') do %>content from block<% end %>"
+        )
+      ).to eq(<<~eos)
+        template some content
+
+        content from block
+
+        more content
+      eos
     end
-  end
 
-  describe 'attributes with default value' do
-    let(:component_class) do
-      Class.new(Components::Rails::Component) do
-        self.defaults = {some_attribute: 'default value'}
-
-        def cache_key
-          '123'
-        end
-
-        def show
-          render inline: <<~eos
-            some content
-
-            <%= some_attribute %>
-
-            more content
-          eos
-        end
+    describe 'block_content in #cache_key' do
+      subject do
+        component_class.new(nil, ActionView::Base.new, :show, an_attribute: :value, block: proc { 'the block' })
       end
-    end
 
-    context 'with attribute' do
-      it 'uses the value' do
-        expect(component_class.render_action(:show, ActionView::Base.new, some_attribute: 'given val')).to eq(<<~eos)
-          some content
-
-          given val
-
-          more content
-        eos
-      end
-    end
-
-    context 'without attribute' do
-      it 'uses the default value' do
-        expect(component_class.render_action(:show, ActionView::Base.new)).to eq(<<~eos)
-          some content
-
-          default value
-
-          more content
-        eos
+      it 'works' do
+        expect(subject.cache_key).to eq([:value, 'the block'])
       end
     end
   end
