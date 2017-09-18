20fde7b558a1ffb102b489f83cf958eaca38f711
Joe Osburn <joe+desiringgod@jnodev.com>
2017-09-18 15:00:17 -0500

    Update rails_helper to include support files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,3 +17,18 @@ ENV['RAILS_ENV'] ||= 'test'
 class ApplicationController < ActionController::Base; end
 
 require 'components-rails'
+
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that end
+# in _spec.rb will both be required and run as specs, causing the specs to be
+# run twice. It is recommended that you do not name files matching this glob to
+# end with _spec.rb. You can configure this pattern with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+#
+# The following line is provided for convenience purposes. It has the downside
+# of increasing the boot-up time by auto-requiring all files in the support
+# directory. Alternatively, in the individual `*_spec.rb` files, manually
+# require only the support files necessary.
+#
+Dir['./spec/support/**/*.rb'].each { |f| require f }
